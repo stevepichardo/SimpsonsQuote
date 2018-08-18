366cac8f8e6e3c3d81929eb1dcf33560801bc6b2
Steven Pichardo <stevenpichardo@stevens-imac.home>
2018-08-18 00:49:41 -0400

    logo img added .. also has a preloader

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-const paragraph = document.getElementById('quoteSaying');
-const authorParagraph = document.getElementById('quoteAuthor');
-const getQuoteBtn = document.getElementById('getNew');
-const characterImage = document.getElementById('characterImg');
+document.onreadystatechange = function() {
+  if (document.readyState === 'interactive') {
+    const paragraph = document.getElementById('quoteSaying');
+    const authorParagraph = document.getElementById('quoteAuthor');
+    const quoteImgContainer = document.querySelector('.quote-img');
+    const loadingContainer = document.querySelector('.loading');
+    const getQuoteBtn = document.getElementById('getNew');
+    const characterImage = document.getElementById('characterImg');
 
-function getQuote() {
-  axios
-    .get('https://thesimpsonsquoteapi.glitch.me/quotes')
-    .then(function(response) {
-      const { quote, character, image } = response.data[0];
+    quoteImgContainer.className.includes('hidden') ? getQuote() : displayData();
 
-      paragraph.innerHTML = quote;
+    function getQuote() {
+      quoteImgContainer.classList.add('hidden');
+      loadingContainer.classList.remove('hidden');
+
+      axios
+        .get('https://thesimpsonsquoteapi.glitch.me/quotes')
+        .then(function(response) {
+          let { quote, character, image } = response.data[0];
+          displayData(quote, character, image);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+
+    function displayData(quote, character, image) {
+      loadingContainer.classList.add('hidden');
+      quoteImgContainer.classList.remove('hidden');
+
+      paragraph.innerHTML = `"${quote}"`;
       authorParagraph.innerHTML = `- ${character}`;
       characterImage.src = image;
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
+    }
 
-// On Document Load Generate Quote
-window.onload = getQuote;
+    // On Click Generate new Quote
+    getQuoteBtn.addEventListener('click', getQuote);
 
-// On Click Generate new Quote
-
-getQuoteBtn.addEventListener('click', getQuote);
+    // On click tweet
+  }
+};
